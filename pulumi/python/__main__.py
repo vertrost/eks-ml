1b68ec19f04c058b584364d56aa084aa0048615d
Sergei Zhekpisov <7286747+vertrost@users.noreply.github.com>
2024-07-14 00:43:55 +0100

    add gpu nodegroup

--- pulumi/python/__main__.py
+++ pulumi/python/__main__.py
@@ -1,10 +1,7 @@
 import iam
 import vpc
-import utils
 import pulumi
 from pulumi_aws import eks
-
-## EKS Cluster
 
 eks_cluster = eks.Cluster(
     'eks-cluster',
@@ -19,17 +16,17 @@ eks_cluster = eks.Cluster(
     ),
 )
 
-eks_node_group = eks.NodeGroup(
-    'eks-node-group',
+eks_node_group_default = eks.NodeGroup(
+    'eks-node-group-default',
     cluster_name=eks_cluster.name,
-    node_group_name='pulumi-eks-nodegroup',
-    # instance_types=["t3.medium"],
-    # capacity_type=["SPOT"],
-    # ami_type="AL2_x86_64",
+    node_group_name='pulumi-eks-nodegroup-default',
+    instance_types=["t3.medium"],
+    capacity_type="SPOT",
+    ami_type="AL2_x86_64",
     node_role_arn=iam.ec2_role.arn,
     subnet_ids=vpc.subnet_ids,
     tags={
-        'Name': 'pulumi-cluster-nodeGroup',
+        'Name': 'pulumi-cluster-nodeGroup-default',
     },
     scaling_config=eks.NodeGroupScalingConfigArgs(
         desired_size=1,
@@ -38,5 +35,28 @@ eks_node_group = eks.NodeGroup(
     ),
 )
 
-pulumi.export('cluster-name', eks_cluster.name)
-pulumi.export('kubeconfig', utils.generate_kube_config(eks_cluster))
+eks_node_group_gpu = eks.NodeGroup(
+    'eks-node-group-gpu',
+    cluster_name=eks_cluster.name,
+    node_group_name='pulumi-eks-nodegroup-gpu',
+    instance_types=["g4dn.xlarge"],
+    capacity_type="SPOT",
+    ami_type="BOTTLEROCKET_x86_64_NVIDIA",
+    node_role_arn=iam.ec2_role.arn,
+    subnet_ids=vpc.subnet_ids,
+    tags={
+        'Name': 'pulumi-cluster-nodeGroup-default',
+    },
+    scaling_config=eks.NodeGroupScalingConfigArgs(
+        desired_size=1,
+        max_size=2,
+        min_size=1,
+    ),
+)
+
+# make private subnets for nodegroups
+# deploy gpu-operator
+# deploy app
+# check app
+# add taints
+# add tolerations
